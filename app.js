cc423ec69380fea5b865ecf406c23a3730fe1320
twboyd <104265960+twboyd@users.noreply.github.com>
2024-05-15 14:31:24 -0400

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname * '/app_server/views/partials');
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.index);
+  
 
 module.exports = router;
