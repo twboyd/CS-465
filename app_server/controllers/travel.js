e3a0e5e63e07d8501d4b3a565fb6ee7b6ce169f1
twboyd <104265960+twboyd@users.noreply.github.com>
2024-05-22 15:30:38 -0400

    Module 3 Completed Basline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var trips =
-JSON.parse(fs.readFileSync('.data/trips.json','utf8'));
+JSON.parse(fs.readFileSync('data/trips.json','utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
