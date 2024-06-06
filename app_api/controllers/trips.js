2c67569f9cc9110b5383447c7cce918dd6e513c5
twboyd <104265960+twboyd@users.noreply.github.com>
2024-06-06 16:54:11 -0400

    commit

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); // register model
+const Model = mongoose.model('trips');
+
+// GET: /trips - lists all the trips
+// Regardless of outcome, response must include HTML status code
+// and JSON ,essage to the requesting client
+const tripsList = async(req, res) => {
+    const q = await Model
+        .find({}) //No filter, return all records
+        .exec();
+
+        // Uncomment the following line to show results of querey
+        // on the console
+        // console.log(q);
+
+        if(!q)
+            {
+                return res
+                .status(404)
+                .json(err);
+            } else {
+                return res
+                .status(200)
+                .json(q);
+            }
+};
+
+const tripsFindByCode = async(req, res) => {
+    const q = await Model
+        .find({"code" : req.params.tripCode}) // return single record
+        .exec();
+
+        // Uncomment the following line to show results of querey
+        // on the console
+        // console.log(q);
+
+        if(!q)
+            {
+                return res
+                .status(404)
+                .json(err);
+            } else {
+                return res
+                .status(200)
+                .json(q);
+            }
+};
+
+module.exports = {
+    tripsList,
+    tripsFindByCode
+};
